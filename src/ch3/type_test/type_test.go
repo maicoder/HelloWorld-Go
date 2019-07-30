38d90f7d6edb1633464b53c3f8b6a14d4ceefa69
0xaeeeee <hkdjcmc@msn.cn>
2019-07-30 23:48:34 +0800

    Update type_test.go

--- src/ch3/type_test/type_test.go
+++ src/ch3/type_test/type_test.go
@@ -12,3 +12,16 @@ func TestImplicit(t *testing.T) {
 	c = MyInt(b)
 	t.Log(a, b, c)
 }
+
+func TestPoint(t *testing.T)  {
+	a := 1
+	aPtr := &a
+	t.Log(a, aPtr)
+	t.Logf("%T %T", a,aPtr)
+}
+
+func TestString(t *testing.T)  {
+	var s string
+	t.Log("*" + s + "*")
+	t.Log(len(s))
+}
