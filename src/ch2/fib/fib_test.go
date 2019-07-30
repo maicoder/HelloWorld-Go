78f5d96f06f121f34b15346d09d3aa01fd42bb8c
0xaeeeee <hkdjcmc@msn.cn>
2019-07-30 22:29:37 +0800

    Create fib_test.go

---
+++ src/ch2/fib/fib_test.go
@@ -0,0 +1,26 @@
+package fib
+
+import (
+	"testing"
+)
+
+func TestFibList(t *testing.T)  {
+	//var a int = 1
+	//var b int = 1
+	//var {
+	//	a int = 1
+	//	b     = 1
+	//}
+	a := 1
+	b := 1
+	//fmt.Print(a)
+	t.Log(a)
+	for i := 0; i < 5; i++ {
+		//fmt.Print(" ", b)
+		t.Log(" ", b)
+		tmp := a
+		a = b
+		b = tmp + a
+	}
+	//fmt.Println()
+}
