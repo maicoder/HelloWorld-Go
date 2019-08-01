116709907a1d7c4d821b4549cf3b64cfae52d282
0xaeeeee <hkdjcmc@msn.cn>
2019-08-02 01:11:15 +0800

    Create array_text.go

---
+++ src/ch6/array_test/array_test.go
@@ -0,0 +1,26 @@
+package array_test
+
+import "testing"
+
+func TestArrayInit(t *testing.T) {
+	var arr [3]int
+	arr1 := [4]int{1, 2, 3, 4}
+	arr2 := [...]int{1, 2, 3, 5}
+	arr1[1] = 5
+	t.Log(arr[1], arr[2])
+	t.Log(arr1, arr2)
+}
+
+func TestArrayTravel(t *testing.T) {
+	arr3 := [...]int{1, 3, 4, 5}
+	for i := 0; i < len(arr3); i++ {
+		t.Log(arr3[i])
+	}
+	for idx, e := range arr3 {
+		t.Log(idx, e)
+	}
+	t.Log(arr3[:])
+	t.Log(arr3[1:])
+	t.Log(arr3[2:3])
+	t.Log(arr3[:3])
+}
