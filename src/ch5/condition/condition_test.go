635fdf25b2659b192f0da7c1ed244dcb4e7fb0cd
0xaeeeee <hkdjcmc@msn.cn>
2019-08-02 00:56:50 +0800

    Update condition_test.go

--- src/ch5/condition/condition_test.go
+++ src/ch5/condition/condition_test.go
@@ -9,3 +9,24 @@ func TestIfMultiSec(t *testing.T) {
 		t.Log("1==1")
 	}
 }
+
+func TestSwitchMultiCase(t *testing.T)  {
+	for i:=0; i<5; i++ {
+		//switch i {
+		//case 0, 2:
+		//	t.Log("Even")
+		//case 1, 3:
+		//	t.Log("Odd")
+		//default:
+		//	t.Log("unknown")
+		//}
+		switch {
+		case i%2 == 0:
+			t.Log("Even")
+		case i%2==1:
+			t.Log("Odd")
+		default:
+			t.Log("unknown")
+		}
+	}
+}
