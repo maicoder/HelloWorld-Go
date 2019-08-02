e9a100c3619f2a5c7bfb5cecacd0087101831d47
0xaeeeee <hkdjcmc@msn.cn>
2019-08-02 22:26:57 +0800

    Update func_test.go

--- src/ch10/func/func_test.go
+++ src/ch10/func/func_test.go
@@ -13,15 +13,15 @@ func returnMultiValues() (int, int) {
 
 func timeSpent(inner func(op int) int) func(op int) int {
 	return func(n int) int {
-		start:=time.Now()
-		ret:=inner(n)
+		start := time.Now()
+		ret := inner(n)
 		fmt.Println("time spent:", time.Since(start).Seconds())
 		return ret
 	}
 }
 
 func slowFun(op int) int {
-	time.Sleep(time.Second*1)
+	time.Sleep(time.Second * 1)
 	return op
 }
 
@@ -30,4 +30,28 @@ func TestFn(t *testing.T) {
 	t.Log(a)
 	tsSF := timeSpent(slowFun)
 	t.Log(tsSF(10))
+}
+
+func Sum(ops ...int) int {
+	ret := 0
+	for _, op := range ops {
+		ret += op
+	}
+	return ret
+}
+
+func TestVarParam(t *testing.T) {
+	t.Log(Sum(1, 2, 3, 4))
+	t.Log(Sum(1, 2, 3, 4, 5))
+}
+
+func Clear() {
+	fmt.Println("Clear resources.")
+}
+
+func TestDefer(t *testing.T) {
+	defer Clear()
+	fmt.Println("Start")
+	panic("err")
+	//fmt.Println("End")
 }
