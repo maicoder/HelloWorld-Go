1081cef304c949397d21c38be0c7b08754a7c51c
0xaeeeee <hkdjcmc@msn.cn>
2019-08-03 22:07:01 +0800

    Create customer_type.go

---
+++ src/ch11/customer_type/customer_type_test.go
@@ -0,0 +1,28 @@
+package customer_type
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type Intconv func(op int) int
+
+func timeSpent(inner Intconv) Intconv {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+		fmt.Println("time spent:", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFun(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func TestFn(t *testing.T) {
+	tsSF := timeSpent(slowFun)
+	t.Log(tsSF(10 ))
+}
