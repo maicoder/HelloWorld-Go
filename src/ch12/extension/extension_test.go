2bfee0610ea6cd3d3614f711e3da8923f8b3180c
0xaeeeee <hkdjcmc@msn.cn>
2019-08-04 10:12:41 +0800

    Create extension_test.go

---
+++ src/ch12/extension/extension_test.go
@@ -0,0 +1,43 @@
+package extension
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Pet struct {
+
+}
+
+func (p *Pet) Speak() {
+	fmt.Print("...")
+}
+
+func (p *Pet) SpeakTo(host string) {
+	p.Speak()
+	fmt.Println(" ", host)
+}
+
+type Dog struct {
+	//p *Pet
+	Pet
+}
+
+//func (d *Dog) Speak() {
+//	fmt.Println("Wang!")
+//}
+//
+//func (d *Dog) SpeakTo(host string) {
+//	//d.p.SpeakTo(host)
+//	d.Speak()
+//	fmt.Println("", host)
+//}
+
+func (d *Dog) Speak() {
+	fmt.Print("Wang!")
+}
+
+func TestDog(t *testing.T)  {
+	dog := new(Dog)
+	dog.SpeakTo("Jack")
+}
