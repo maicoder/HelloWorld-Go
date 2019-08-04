b506e5ba6bf03e4539811572de9a4c5ee01d92a1
0xaeeeee <hkdjcmc@msn.cn>
2019-08-04 23:27:54 +0800

    Update empty_interface_test.go

--- src/ch13/empty_interface/empty_interface_test.go
+++ src/ch13/empty_interface/empty_interface_test.go
@@ -6,15 +6,24 @@ import (
 )
 
 func DoSomething(p interface{}) {
-	if i, ok := p.(int); ok {
-		fmt.Println("Integer", i)
-		return
+	//if i, ok := p.(int); ok {
+	//	fmt.Println("Integer", i)
+	//	return
+	//}
+	//if s, ok := p.(string); ok {
+	//	fmt.Println("string", s)
+	//	return
+	//}
+	//fmt.Println("Unknown Type")
+
+	switch v:=p.(type) {
+	case int:
+		fmt.Println("integer", v)
+	case string:
+		fmt.Println("string", v)
+	default:
+		fmt.Println("Unknown Type")
 	}
-	if s, ok := p.(string); ok {
-		fmt.Println("string", s)
-		return
-	}
-	fmt.Println("Unknown Type")
 }
 
 func TestEmptyInterfaceAssertion(t *testing.T) {
