b114ae0860b22359c3319831acb1c213da2d1eeb
0xaeeeee <hkdjcmc@msn.cn>
2019-08-06 23:32:38 +0800

    Update panic_recover_test.go

--- src/ch14/panic_recover/panic_recover_test.go
+++ src/ch14/panic_recover/panic_recover_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func TestPanicVxExit(t *testing.T) {
-	//defer func() {
-	//	if err := recover(); err != nil {
-	//		fmt.Println("recover from: ", err)
-	//	}
-	//}()
-
 	defer func() {
-		fmt.Println("Finally!")
+		if err := recover(); err != nil {
+			fmt.Println("recover from: ", err)
+		}
 	}()
+
+	//defer func() {
+	//	fmt.Println("Finally!")
+	//}()
 
 	fmt.Println("Start")
 	panic(errors.New("Something wrong!"))
