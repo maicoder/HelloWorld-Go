579412f007a3fb21ac2c918109a839bb266fb86d
0xaeeeee <hkdjcmc@msn.cn>
2019-08-08 20:54:50 +0800

    Update first_response

--- src/ch22/util_anyone_reply/first_response_test.go
+++ src/ch22/util_anyone_reply/first_response_test.go
@@ -14,7 +14,8 @@ func runTask(id int) string {
 
 func FirstResponse() string {
 	numOfRunner := 10
-	ch := make(chan string)
+	//ch := make(chan string)
+	ch := make(chan string, numOfRunner) //使用bufferchannel
 	for i := 0; i < numOfRunner; i++ {
 		go func(i int) {
 			ret := runTask(i)
@@ -27,6 +28,7 @@ func FirstResponse() string {
 func TestFirstResponse(t *testing.T) {
 	t.Log("Before: ", runtime.NumGoroutine())
 	t.Log(FirstResponse())
-	t.Log(time.Second * 1)
+	//t.Log(time.Second * 1)
+	time.Sleep(time.Second * 1)
 	t.Log("After: ", runtime.NumGoroutine())
 }
