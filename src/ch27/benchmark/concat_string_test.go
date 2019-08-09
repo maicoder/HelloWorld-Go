1a170421bc3acee283325047752d0bee3d8a8c97
0xaeeeee <hkdjcmc@msn.cn>
2019-08-09 22:29:46 +0800

    Add concat_string

---
+++ src/ch27/benchmark/concat_string_test.go
@@ -0,0 +1,39 @@
+package benchmark
+
+import (
+	"bytes"
+	"testing"
+)
+import "github.com/stretchr/testify/assert"
+
+func TestConcattringByAdd(t *testing.T) {
+	assert := assert.New(t)
+	elems := []string{"1", "2", "3", "4", "5"}
+	ret := ""
+	for _, elem := range elems {
+		ret += elem
+	}
+	assert.Equal("12345", ret)
+}
+
+func TestConcatStringBytesBuffer(t *testing.T) {
+	assert := assert.New(t)
+	var buf bytes.Buffer
+	elems := []string{"1", "2", "3", "4", "5"}
+	for _, elem := range elems {
+		buf.WriteString(elem)
+	}
+	assert.Equal("12345", buf.String())
+}
+
+func BenchmarkConcatStringByAdd(b *testing.B) {
+	elems := []string{"1", "2", "3", "4", "5"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		for _, elem := range elems {
+			buf.WriteString(elem)
+		}
+	}
+	b.StartTimer()
+}
