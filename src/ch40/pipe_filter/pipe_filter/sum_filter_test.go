68290dba89177c66ef8abf06065693e95297e0ed
0xaeeeee <hkdjcmc@msn.cn>
2019-08-12 18:42:49 +0800

    Add sum_filter

---
+++ src/ch40/pipe_filter/pipe_filter/sum_filter_test.go
@@ -0,0 +1,24 @@
+package pipefilter
+
+import "testing"
+
+func TestSumElems(t *testing.T)  {
+	sf := NewSumFilter()
+	ret, err := sf.Process([]int{1, 2, 3})
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := 5
+	if ret != expected {
+		t.Fatalf("the expected is %d, but actual is %d", expected, ret)
+	}
+}
+
+func TestWrongInputForSumFilter(t *testing.T)  {
+	sf := NewSumFilter()
+	_, err := sf.Process([]float32{1.1, 2.2, 3.3 })
+
+	if err == nil {
+		t.Fatal("An error is xpected")
+	}
+}
