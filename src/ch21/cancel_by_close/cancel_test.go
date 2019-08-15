7deb1b48ac1ea7ce5e72b91c48f543d955b3a560
0xaeeeee <hkdjcmc@msn.cn>
2019-08-15 23:46:20 +0800

    Update cancel_test.go

--- src/ch21/cancel_by_close/cancel_test.go
+++ src/ch21/cancel_by_close/cancel_test.go
@@ -15,15 +15,15 @@ func isCancelled(cancelChan chan struct{}) bool {
 	}
 }
 
-func cancel_1(cancelChan chan struct{})  {
+func cancel_1(cancelChan chan struct{}) {
 	cancelChan <- struct{}{}
 }
 
-func cancel_2(cancelChan chan struct{})  {
+func cancel_2(cancelChan chan struct{}) {
 	close(cancelChan)
 }
 
-func TestCancel(t *testing.T)  {
+func TestCancel(t *testing.T) {
 	cancelChan := make(chan struct{}, 0)
 	for i := 0; i < 5; i++ {
 		go func(i int, cancelCh chan struct{}) {
