2537095f40f27b8b96913f0fbf3d88adef624714
0xaeeeee <hkdjcmc@msn.cn>
2019-10-08 21:27:44 +0800

    Update http_router.go

--- src/ch45/http_router/http_router.go
+++ src/ch45/http_router/http_router.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io"
 	"log"
 	"net/http"
 )
+
+type myHandler struct {}
+
+func (h *myHandler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	io.WriteString(w, "HelloWorld!\n")
+}
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Welcome!\n")
@@ -16,9 +23,18 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 }
 
 func main() {
+	/*
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
 
 	log.Fatal(http.ListenAndServe(":8888", router))
+
+	 */
+	mux := http.NewServeMux()
+
+	h := new(myHandler)
+	mux.Handle("/foo", h)
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", mux)
 }
